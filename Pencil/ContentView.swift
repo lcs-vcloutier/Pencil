3be82114c5457ef34407ad1fdddd03e9c7350889
Vincent Cloutier <vincentxcloutier@gmail.com>
2020-11-02 17:21:35 -0500

    added ink and eraser

--- Pencil/ContentView.swift
+++ Pencil/ContentView.swift
@@ -30,7 +30,7 @@ struct Home : View {
     @State var colorPicker = false
     
     // Default is pen
-     
+    
     var body: some View{
         
         NavigationView{
@@ -39,6 +39,29 @@ struct Home : View {
             DrawingView(canvas: $canvas)
                 .navigationTitle("Drawing")
                 .navigationBarTitleDisplayMode(.inline)
+                .navigationBarItems(leading: Button(action: {
+                    
+                    // saving Image...
+                    
+                    
+                }, label: {
+                    
+                    Image(systemName: "square.and.arrow.down.fill")
+                        .font(.title)
+                }),trailing: HStack(spacing: 15){
+                    
+                    Button(action: {
+                        
+                        // erase tool....
+                        
+                        isDraw = false
+                        
+                    }) {
+                        
+                        Image(systemName: "pencil.slash")
+                            .font(.title)
+                    }
+                })
         }
     }
 }
@@ -46,7 +69,11 @@ struct DrawingView : UIViewRepresentable {
     
     // To capture drawing for saving into albums
     @Binding var canvas : PKCanvasView
-    
+    @Binding var isDraw : Bool
+        
+    let ink = PKInkingTool(.pencil, color: .black)
+    let eraser = PKEraserTool(.bitmap)
+   
     func makeUIView(context: Context) -> PKCanvasView{
         
         canvas.drawingPolicy = .anyInput
