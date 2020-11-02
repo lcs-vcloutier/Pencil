9703a81d18b85da0856bcc20a7d5516b74ed11ea
Vincent Cloutier <vincentxcloutier@gmail.com>
2020-11-02 17:29:42 -0500

    app functional, need to clean up code and change menu image

--- Pencil/ContentView.swift
+++ Pencil/ContentView.swift
@@ -36,7 +36,7 @@ struct Home : View {
         NavigationView{
             
             // Drawing View
-            DrawingView(canvas: $canvas, isDraw: $isDraw)
+            DrawingView(canvas: $canvas, isDraw: $isDraw,type: $type,color: $color)
                 .navigationTitle("Drawing")
                 .navigationBarTitleDisplayMode(.inline)
                 .navigationBarItems(leading: Button(action: {
@@ -62,99 +62,98 @@ struct Home : View {
                             .font(.title)
                     }
                     
-                                       // Menu For InkType And Color....
-                                       
-                                       Menu {
-                                           
-                                           // Color Picker...
-                                           
-                                           Button(action: {
-                                               
-                                               colorPicker.toggle()
-                                               
-                                           }) {
-                                               
-                                               Label {
-                                                   
-                                                   Text("Color")
-                                                   
-                                               } icon: {
-                                                   
-                                                   Image(systemName: "eyedropper.full")
-                                               }
-                                           }
-                                           
-                                           Button(action: {
-                                               
-                                               // changing type....
-                                               
-                                               isDraw = true
-                                               type = .pencil
-                                               
-                                           }) {
-                                               
-                                               Label {
-                                                   
-                                                   Text("Pencil")
-                                                   
-                                               } icon: {
-                                                   
-                                                   Image(systemName: "pencil")
-                                               }
-                                               
-                                           }
-                                           
-                                           Button(action: {
-                                               
-                                               isDraw = true
-                                               type = .pen
-                                               
-                                           }) {
-                                               
-                                               Label {
-                                                   
-                                                   Text("Pen")
-                                                   
-                                               } icon: {
-                                                   
-                                                   Image(systemName: "pencil.tip")
-                                               }
-                                               
-                                           }
-                                           
-                                           Button(action: {
-                                               
-                                               isDraw = true
-                                               type = .marker
-                                               
-                                           }) {
-                                               
-                                               Label {
-                                                   
-                                                   Text("Marker")
-                                                   
-                                               } icon: {
-                                                   
-                                                   Image(systemName: "highlighter")
-                                               }
-                                               
-                                           }
-                                           
-                                       } label: {
-                                           
-                                           Image("menu")
-                                               .resizable()
-                                               .frame(width: 22, height: 22)
-                                       }
-
-                                   })
-                                   .sheet(isPresented: $colorPicker) {
-                                       
-                                       ColorPicker("Pick Color", selection: $color)
-                                           .padding()
-                                   }
-                           }
-                       }
+                    // Menu For InkType And Color....
+                    
+                    Menu {
+                        
+                        // Color Picker
+                        Button(action: {
+                            
+                            colorPicker.toggle()
+                            
+                        }) {
+                            
+                            Label {
+                                
+                                Text("Color")
+                                
+                            } icon: {
+                                
+                                Image(systemName: "eyedropper.full")
+                            }
+                        }
+                        
+                        Button(action: {
+                            
+                            // changing type....
+                            
+                            isDraw = true
+                            type = .pencil
+                            
+                        }) {
+                            
+                            Label {
+                                
+                                Text("Pencil")
+                                
+                            } icon: {
+                                
+                                Image(systemName: "pencil")
+                            }
+                            
+                        }
+                        
+                        Button(action: {
+                            
+                            isDraw = true
+                            type = .pen
+                            
+                        }) {
+                            
+                            Label {
+                                
+                                Text("Pen")
+                                
+                            } icon: {
+                                
+                                Image(systemName: "pencil.tip")
+                            }
+                            
+                        }
+                        
+                        Button(action: {
+                            
+                            isDraw = true
+                            type = .marker
+                            
+                        }) {
+                            
+                            Label {
+                                
+                                Text("Marker")
+                                
+                            } icon: {
+                                
+                                Image(systemName: "highlighter")
+                            }
+                            
+                        }
+                        
+                    } label: {
+                        
+                        Image(systemName: "pencil.slash")
+                            .resizable()
+                            .frame(width: 22, height: 22)
+                    }
+                    
+                })
+                .sheet(isPresented: $colorPicker) {
+                    
+                    ColorPicker("Pick Color", selection: $color)
+                        .padding()
+                }
+        }
+    }
     
     func SaveImage(){
         
@@ -175,11 +174,11 @@ struct DrawingView : UIViewRepresentable {
     @Binding var type : PKInkingTool.InkType
     @Binding var color : Color
     // updating inkType....
-       
-       var ink : PKInkingTool{
-           
-           PKInkingTool(type, color: UIColor(color))
-       }
+    
+    var ink : PKInkingTool{
+        
+        PKInkingTool(type, color: UIColor(color))
+    }
     let eraser = PKEraserTool(.bitmap)
     
     func makeUIView(context: Context) -> PKCanvasView{
