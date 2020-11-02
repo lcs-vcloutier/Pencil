7b55f76b92a5e9a57eca12b145fbd4353557330e
Vincent Cloutier <vincentxcloutier@gmail.com>
2020-11-02 17:04:56 -0500

    Initial Commit

---
+++ Pencil/PencilApp.swift
@@ -0,0 +1,17 @@
+//
+//  PencilApp.swift
+//  Pencil
+//
+//  Created by Cloutier, Vincent on 2020-11-02.
+//
+
+import SwiftUI
+
+@main
+struct PencilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
